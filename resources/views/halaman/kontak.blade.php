520cfa54dcec7419b0470f2d1851d41264a657e0
Ridwan Yusuf Rifaldi <ridwanyusuf3663@gmail.com>
2023-06-08 23:46:09 +0700

    Tambah Kontak

--- resources/views/halaman/kontak.blade.php
+++ resources/views/halaman/kontak.blade.php
@@ -1,12 +1,13 @@
 @extends('layout/project')
 @section('konten')
 <h1> {{ $judul }} </h1>
-<p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat explicabo necessitatibus placeat 
+<p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat explicabo necessitatibus placeat
     voluptatem minima, molestias vel eaque cum dolorum harum totam officia dolor maiores omnis quos. Ut natus qui neque. </p>
-    <p>
-        <ul>
-            <li> Email: {{ $kontak['email']}} </li>
-            <li> Youtube: {{ $kontak['youtube']}} </li>
+<p>
+<ul>
+    <li> KONTAK KAMI </li>
+    <li> Email: {{ $kontak['email']}} </li>
+    <li> Youtube: {{ $kontak['youtube']}} </li>
 </ul>
 </p>
 @endsection
